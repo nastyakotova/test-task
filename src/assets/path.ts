f57a91082c1509863146d4f7005dc2bf337e7036
Kotova Anastasia <nastya-kotikm@yandex.ru>
2024-02-22 19:43:48 +0300

    remove route

--- src/assets/path.ts
+++
@@ -1,3 +0,0 @@
-export const Path = {
-    home: '/',
-} as const;
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 
 import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import { store } from './store/store';
-import { Path } from './assets/path';
 import { Home } from './pages/home';
 
 import theme from './styles/theme.json';
 import './styles/index.css';
 import { dataSlice } from './store/features/dataSlice';
-
-const router = createBrowserRouter([
-    {
-        path: Path.home,
-        element: <Home />,
-    },
-]);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -28,7 +19,7 @@ root.render(
         <Provider store={store}>
             <ApiProvider api={dataSlice}>
                 <ConfigProvider theme={theme}>
-                    <RouterProvider router={router} />
+                    <Home />
                 </ConfigProvider>
             </ApiProvider>
         </Provider>
